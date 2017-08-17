9415b8b8252cae9f9703c5105b9e399e512279f4
mgaspari <marcoanthonygaspari@gmail.com>
2017-08-16 21:47:02 -0400

    Done.

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,1 +1,13 @@
 # Create your CostumeStore class here
+# class CostumeStore < ActiveRecord::Base
+#   def change
+#     create_table :students do |t|
+#             t.string :name
+#           end
+#   end
+#
+# end
+class CostumeStore < ActiveRecord::Base
+
+
+end
